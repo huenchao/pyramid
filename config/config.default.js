ea12c90389f240a22630099e8182274c6425974b
lianshan <lianshan@pinduoduo.com>
2020-05-19 21:52:23 +0800

    feat: 🎸 just for test how to make a framework based egg
    just for test how to make a framework based egg

---
+++ config/config.default.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  config.keys = 'keys';
+
+  /**
+   * some description
+   * @member Config#test
+   * @property {String} key - some description
+   */
+  config.test = {
+    key: appInfo.name + '_123456',
+  };
+
+
+  return config;
+};
